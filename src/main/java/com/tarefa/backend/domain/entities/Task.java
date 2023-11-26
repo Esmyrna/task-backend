3c4276026721653aa0c399ab9b1766f79d83fa0d
Esmyrna <103766811+Esmyrna@users.noreply.github.com>
2023-11-26 18:03:44 -0300

    Update Task.java

--- src/main/java/com/tarefa/backend/domain/entities/Task.java
+++ src/main/java/com/tarefa/backend/domain/entities/Task.java
@@ -19,27 +19,5 @@ public class Task {
     private String description;
     private boolean completed;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public boolean isCompleted() {
-        return completed;
-    }
-
-    public void setCompleted(boolean completed) {
-        this.completed = completed;
-    }
+    
 }
