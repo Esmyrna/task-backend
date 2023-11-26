1507c76100e3a0cda789e0e52ab1f956a6b72ed3
Esmyrna <Esmyrnaoliveira13@gmail.com>
2023-11-25 23:42:29 -0300

    implementação do meu service task

--- src/main/java/com/tarefa/backend/domain/entities/services/ITaskService.java
+++ src/main/java/com/tarefa/backend/domain/entities/services/ITaskService.java
@@ -1,7 +1,9 @@
 package com.tarefa.backend.domain.entities.services;
 
 import com.tarefa.backend.domain.entities.Task;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ITaskService {
     Task createTask(String description);
     void markAsCompleted(Long id);
